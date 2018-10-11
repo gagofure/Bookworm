b5c80f617bebe290b2807d714605531415ef4848
gagofure <godspower.agofure@gmail.com>
2018-10-11 23:20:13 +0100

    add new notes to board capabilites
    added ability to add new notes to the board and each note id is now generated by a function

--- src/Board.js
+++ src/Board.js
@@ -1,29 +1,38 @@
 import React, { Component } from 'react';
 import Note from './Note';
+import { FaPlusCircle } from 'react-icons/fa';
+
 
 class Board extends Component {
 	constructor(props) {       // render note based on some state data
 		super(props)
 		this.state = {
-			notes: [		// an arry that list numbers of note in our App
-				{
-					id: 0,
-					note: "Call Lisa"
-				},
-				{
-					id: 1,
-					note: "Email Jon"
-				},
-				{
-					id: 2,
-					note: "Buy some Kitkat"
-				}
-			]
+			notes: [] // an arry that list numbers of note in our App
 		}
+		this.add = this.add.bind(this);
 		this.eachNote = this.eachNote.bind(this);
 		this.update = this.update.bind(this);
 		this.remove = this.remove.bind(this);
+		this.nextId = this.nextId.bind(this);
 	}
+		// Add new Note to the board 
+		add(text){
+			this.setState(prevState => ({
+				notes: [
+					...prevState.notes,
+					{
+						id: this.nextId(), 
+						note: text
+					}
+				]
+			}))
+		}
+
+		// generates id for a new note added or created
+		nextId(){
+			this.uniqueId || 0
+			return this.uniqueId++ 
+		}
 
 	//  Update/edit the existing note || The state of the note is held in Board component.
 	// Update fuction will help us send the newText from it's child, Note component.
@@ -45,7 +54,9 @@ class Board extends Component {
 	}
 
 
-	//render note based on our dynamic data
+
+
+	//renders note based on our dynamic data
 	//for each note in state display the note/notes
 	eachNote(note, i) {
 		return (
@@ -55,15 +66,18 @@ class Board extends Component {
 				onRemove={this.remove}>
 				{note.note}
 			</Note>
-		)// onChange get the newText from the note by calling the update Functon 
+		) // onChange get the newText from the note by calling the update Functon
 
 	}
 
-	//.maps maps all the note in state and call each note fuction for every instance of a note 
+	//.maps maps all the note in state and call each note fuction for every instance of a note || this is the board component
 	render() {
 		return (
 			<div className="board" >
 				{this.state.notes.map(this.eachNote)}
+				<button onClick= {this.add.bind (null, "New Note")} id="add">
+					<FaPlusCircle></FaPlusCircle>
+				</button> 					{/* add new note to the board*/}
 			</div>
 		)
 	}
