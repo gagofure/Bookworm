8bc4d72d741e53d22ca08eb45f4e8501fb8ff11a
gagofure <godspower.agofure@gmail.com>
2018-11-09 08:27:55 +0100

    Final Build
    Completed the app and ran "npm run build"

--- src/Note.js
+++ src/Note.js
@@ -15,7 +15,38 @@ class Note extends Component {
         this.save = this.save.bind( this );
         this.renderForm = this.renderForm.bind( this );
         this.renderDisplay = this.renderDisplay.bind( this );
+        this.randomBetween = this.randomBetween.bind(this);
     }
+
+    componentWillMount(){
+        this.style= {
+            right: this.randomBetween(0, window.innerWidth - 150, 'px'),
+            top: this.randomBetween(0,window.innerHeight - 150, 'px'),
+            transform: `rotate(${this.randomBetween(-25, 25, 'deg')})`
+        }
+    }
+    // place the notes randomly on the board Component
+    randomBetween(x,y,s) {
+        return x + Math.ceil(Math.random() * (y-x)) + s
+    }
+
+    componentDidUpdate() {
+        var textArea
+        if (this.state.editing) {
+            textArea = this._newText
+            textArea.focus()
+            textArea.select()
+        }
+
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return (
+            this.props.children !== nextProps.children || this.state !== nextState
+        )
+    }
+
+
 // sets the state of edit
     edit( ) {
         this.setState({
@@ -23,7 +54,7 @@ class Note extends Component {
         })
     }
 //sets the state of save
-    save(e ) {
+    save(e) {
        e.preventDefault();
        this.props.onChange(this._newText.value, this.props.index)
        this.setState({
@@ -39,9 +70,9 @@ class Note extends Component {
 // render the form that takes text input from users
     renderForm () {
              return (
-                <div className="note"> 
+                <div className="note" style={this.style}> 
                     <form onSubmit = {this.save} >
-                        <textarea ref = {input => this._newText = input } />
+                        <textarea ref = {input => this._newText = input } defaultValue={ this.props.children} />
                         <button id='save' ><FaFileAlt/> </button>
                     </form>
                 </div>
@@ -51,7 +82,7 @@ class Note extends Component {
 // displays the Note to the users
  renderDisplay( ) {
         return( 
-               <div className = "note" >
+               <div className = "note" style ={this.style} >
                     <p> {this.props.children} </p>
                     <span>
                         <button onClick = {this.edit} id="edit"> < FaPencilAlt/></button>
@@ -67,4 +98,5 @@ class Note extends Component {
             return this.state.editing ? this.renderForm(): this.renderDisplay() }
                
 }
+
 export default Note;
